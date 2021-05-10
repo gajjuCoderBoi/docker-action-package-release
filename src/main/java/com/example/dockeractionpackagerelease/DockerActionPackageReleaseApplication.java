b1d30263f7f306322318962f617935c382b10dc1
Mohammad Javed <ghazanfar9131@gmail.com>
2021-05-10 00:46:45 -0400

    added endpoint.

--- src/main/java/com/example/dockeractionpackagerelease/DockerActionPackageReleaseApplication.java
+++ src/main/java/com/example/dockeractionpackagerelease/DockerActionPackageReleaseApplication.java
@@ -2,9 +2,17 @@ package com.example.dockeractionpackagerelease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerActionPackageReleaseApplication {
+
+    @GetMapping
+    public String greetings(){
+        return "Welcome";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DockerActionPackageReleaseApplication.class, args);
