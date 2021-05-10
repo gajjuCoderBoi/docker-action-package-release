e0fe3c5a5068f1c18a980be5a8068d5638e447d2
Mohammad Javed <ghazanfar9131@gmail.com>
2021-05-09 23:12:15 -0400

    init

---
+++ src/main/java/com/example/dockeractionpackagerelease/DockerActionPackageReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockeractionpackagerelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerActionPackageReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerActionPackageReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockeractionpackagerelease/DockerActionPackageReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockeractionpackagerelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerActionPackageReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
